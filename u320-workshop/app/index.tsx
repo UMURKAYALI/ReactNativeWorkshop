844b432d656d8bf57ff7dcece800d143d0bc9935
UMURKAYALI <umur.kayali@stu.bilgi.edu.tr>
2025-08-28 20:01:59 +0300

    Login page created

--- u320-workshop/app/index.tsx
+++ u320-workshop/app/index.tsx
@@ -1,11 +1,79 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const LoginPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <Text>Hello World!</Text>
-        </View>
+        <ScrollView style={styles.mainContainer}>
+            <View style={styles.container}>
+                <Image source={require('../assets/images/safir_logo_3.png')} style={styles.logo} />
+                <Text style={styles.title}>Oturum Aç</Text>
+            </View>
+
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder="Kullanıcı Adı" value={username} onChangeText={(username: string) => setUsername(username)}></TextInput>
+                <TextInput style={styles.input} placeholder="Parola" secureTextEntry={true} value={password} onChangeText={(password: string) => setPassword(password)}></TextInput>
+
+                <TouchableOpacity style={styles.touchableButton} onPress={() => { console.log('Touchable Custom Button Pressed') }}>
+                    <View>
+                        <Text style={styles.buttonText}>Giriş Yap</Text>
+                    </View>
+
+                </TouchableOpacity>
+            </View>
+
+            <Button title="Giriş Yap" color={'green'} onPress={() => { console.log('Login button pressed') }}></Button>
+
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: '#ffffff7e'
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 500,
+    },
+    logo: {
+        marginTop: 30,
+        resizeMode: "contain",
+        width: 300,
+        height: 250,
+    },
+    input: {
+        borderWidth: 1,
+        padding: 10,
+        marginTop: 8,
+        borderRadius: 5,
+        width: '90%'
+    },
+    touchableButton: {
+        marginTop: 20,
+        display: 'flex',
+        alignItems: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: 'gray',
+        backgroundColor: 'rgb(40,47,61)',
+        width: '90%'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16
+    }
+});
+
 
 export default LoginPage;  
